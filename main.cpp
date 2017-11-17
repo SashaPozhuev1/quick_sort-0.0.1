3b3c1419181fed333e1bb74efecca5a20c53a9ea
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2017-11-17 12:07:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,119 +6,137 @@
 using namespace std;
 
 bool read(int &size) {
-	string stroka;
-	getline(cin, stroka);
-	istringstream stream(stroka);
+    string stroka;
+    getline(cin, stroka);
+    istringstream stream(stroka);
 
-	bool success = true;
+    bool success = true;
 
-	if (stream >> size && size > 0) {
+    if (stream >> size && size > 0) {
 
-	}
-	else {
-		success = false;
-	}
+    }
+    else {
+        success = false;
+    }
 
-	return success;
+    return success;
 }
 
 bool create(double * & mass, int resultsize) {
-	bool success = true;
+    bool success = true;
 
-	string stroka;
-	getline(cin, stroka);
-	istringstream stream(stroka);
+    string stroka;
+    getline(cin, stroka);
+    istringstream stream(stroka);
 
-	int size = 0;
-	double element;
+    int size = 0;
+    double element;
 
-	mass = new double[resultsize];
-	int i = 0;
-	while (stream >> element) {
-		if (size < resultsize) {
-			mass[i] = element;
-			i++;
-			size++;
-		}
-		else {
-			success = false;
-			break;
-		}
-	}
+    mass = new double[resultsize];
+    int i = 0;
+    while (stream >> element) {
+        if (size < resultsize) {
+            mass[i] = element;
+            i++;
+            size++;
+        }
+        else {
+            success = false;
+            break;
+        }
+    }
 
-	if (resultsize > size) {
-		success = false;
-	}
+    if (resultsize > size) {
+        success = false;
+    }
 
-	return success;
-}
-
-void quick_sort(double * mass, int size) {
-	int j = right_border, next;
-	
-	while (left_border < j) {
-		while (mass[left_border] < mass[j]) {
-			j--;
-		}
-		
-		if (mass[j] <= mass[left_border]) {
-			next = mass[left_border + 1];
-			mass[left_border + 1] = mass[left_border];
-			mass[left_border] = mass[j];
-			mass[j] = next;
-			left_border++;
-		}
-	}
-	/*
-	if (left_border - 1 > size - 1 - left_border) {
-		left_border = 0;
-		right_border = left_border;
-		quick_sort(mass, size, right_border, left_border);
-	}
-	else {
-		right_border = size - 1;
-		quick_sort(mass, size, right_border, left_border);
-	}
-	*/
-	
-
-
+    return success;
 }
 
 void write(double * mass, int size) {
-	for (int i = 0; i < size; i++) {
-		cout << mass[i];
-		if (i != size - 1) {
-			cout << ' ';
-		}
-		else {
-			cout << '\n';
-		}
-	}
+    for (int i = 0; i < size; i++) {
+        cout << mass[i];
+        if (i != size - 1) {
+            cout << ' ';
+        }
+        else {
+            cout << '\n';
+        }
+    }
+}
+
+void quick_sort(double * mass, int size, int right_border, int left_border) {
+    /* int j = right_border, next;
+
+     while (left_border < j) {
+         while (mass[left_border] < mass[j]) {
+             j--;
+         }
+
+         if (mass[j] <= mass[left_border]) {
+             next = mass[j];
+             mass[j] = mass[left_border + 1];
+             mass[left_border + 1] = next;
+             next = mass[left_border + 1];
+             mass[left_border + 1] = mass[left_border];
+             mass[left_border] = next;
+
+             left_border++;
+         }
+     }
+
+     if (left_border - 2 > right_border - left_border) {
+         next = left_border;
+         left_border = 0;
+         right_border = next;
+         quick_sort(mass, size, right_border, left_border);
+     }
+     else {
+         right_border = size - 1;
+         quick_sort(mass, size, right_border, left_border);
+     }
+ */
+    int i = left_border, j = right_border, next;
+    while(left_border < right_border) {
+        while(mass[right_border] > mass[left_border] && left_border < right_border) {
+            right_border--;
+        }
+        if(left_border < right_border && mass[right_border] >= mass[left_border]) {
+            swap(mass[left_border], mass[right_border]);
+            swap(mass[left_border + 1], mass[right_border]);
+
+            left_border++;
+        }
+
+    }
+
+
+
+
 }
 
 void error() {
-	cout << "An error has occured while reading input data.\n";
+    cout << "An error has occured while reading input data.\n";
 }
 
 int main()
 {
-	int size = 0;
-	double * mass = nullptr;
+    int size = 0;
+    double * mass = nullptr;
 
-	if (read(size)) {
-		if (create(mass, size)) {
-			
-			write(mass, size);
-		}
-		else {
-			error();
-		}
-	}
-	else {
-		error();
-	}
+    if (read(size)) {
+        if (create(mass, size)) {
+            quick_sort(mass, size, size - 1, 0);
+            write(mass, size);
+        }
+        else {
+            error();
+        }
+    }
+    else {
+        error();
+    }
 
-	system("pause");
+    system("pause");
     return 0;
 }
